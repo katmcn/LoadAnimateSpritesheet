e69047ebbcd73211c2ddeaf198fb04e5943ae595
katmcn <ekaterina.mcneill@gmail.com>
2023-05-23 12:56:10 +0100

    main changed to sort frames into actions and then loop through images within that action to create an animation. navigate through those animation actions with arrow keys

--- main.py
+++ main.py
@@ -15,32 +15,52 @@ sprite_sheet = animation_functions.SpriteSheet(sprite_sheet_image)
 BG = (50, 50, 50)
 COLOUR = (0, 0, 0)
 
-frame_0 = sprite_sheet.get_image(0, 16, 16, 3, COLOUR)
-frame_1 = sprite_sheet.get_image(1, 16, 16, 3, COLOUR)
-frame_2 = sprite_sheet.get_image(2, 16, 16, 3, COLOUR)
-frame_3 = sprite_sheet.get_image(3, 16, 16, 3, COLOUR)
-frame_4 = sprite_sheet.get_image(4, 16, 16, 3, COLOUR)
-frame_5 = sprite_sheet.get_image(5, 16, 16, 3, COLOUR)
-frame_6 = sprite_sheet.get_image(6, 16, 16, 3, COLOUR)
-frame_7 = sprite_sheet.get_image(7, 16, 16, 3, COLOUR)
+#create animation list
+animation_list = []
+animation_steps = [2, 2, 2, 2]
+action = 0
+last_update = pygame.time.get_ticks()
+animation_cooldown = 600 #milliseconds
+frame = 0
+step_counter = 0
+
+for animation in animation_steps:
+    temp_image_list = []
+    for _ in range(animation):
+        temp_image_list.append(sprite_sheet.get_image(step_counter, 16, 16, 3, COLOUR))
+        step_counter += 1
+    animation_list.append(temp_image_list)
 
 run = True
 while run:
 
+    #Update screen background
     screen.fill(BG)
 
-    screen.blit(frame_0, (0, 0))
-    screen.blit(frame_1, (50, 0))
-    screen.blit(frame_2, (100, 0))
-    screen.blit(frame_3, (150, 0))
-    screen.blit(frame_4, (200, 0))
-    screen.blit(frame_5, (250, 0))
-    screen.blit(frame_6, (300, 0))
-    screen.blit(frame_7, (350, 0))
+    #update animation
+    current_time = pygame.time.get_ticks()
+    if current_time - last_update >= animation_cooldown:
+        frame += 1
+        last_update = current_time
+        if frame >= len(animation_list[action]):
+            frame = 0
 
+    #show image frames
+    screen.blit(animation_list[action][frame], (0, 0))
+
+
+    #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN and action > 0:
+                action -= 1
+                frame = 0
+            if event.key == pygame.K_UP and action < len(animation_list) - 1:
+                action += 1
+                frame = 0
+
 
     pygame.display.update()
 
